7f29f089da8c3821e5019edf83add5a1bbc06e60
root <1020281157@qq.com>
2020-04-11 19:06:20 +0800

    dev

--- FirstStudy/src/HelloWorld.java
+++ FirstStudy/src/HelloWorld.java
@@ -1,12 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(123);
-
-        System.out.println(223);
-        
-        System.out.println(333);
-        System.out.println(433);
-        System.out.println(124124);
         getWork();
     }
 
