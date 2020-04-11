c8fe812aa01cd250d5eebdaf1d66cc41e2870844
root <1020281157@qq.com>
2020-04-11 18:18:40 +0800

    第一次从dev提交

--- FirstStudy/src/HelloWorld.java
+++ FirstStudy/src/HelloWorld.java
@@ -1,13 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(123);
-
-        System.out.println(223);
-        
-        System.out.println(333);
-        System.out.println(433);
         System.out.println("master");
-        System.out.println(124124);
         getWork();
     }
 
